5a86ae3f22bb7987f445db8393a01c7af253fb86
Jolly <52607257+lzy-Jolly@users.noreply.github.com>
2024-09-15 04:04:20 +1000

    Update AWS_lightsail_open_ssh.sh

--- AWS_lightsail_open_ssh.sh
+++ AWS_lightsail_open_ssh.sh
@@ -4,10 +4,10 @@
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
 # 修改 PermitRootLogin 行，去掉井号并设置为 PermitRootLogin yes
-sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 # 修改 PasswordAuthentication 行，去掉井号并设置为 PasswordAuthentication yes
-sed -i 's/#PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 # 查找 /etc/ssh/sshd_config.d 文件夹中以 cloudimg-settings.conf 结尾的文件，并修改 PasswordAuthentication no 为 PasswordAuthentication yes
 for conf_file in /etc/ssh/sshd_config.d/*cloudimg-settings.conf; do
