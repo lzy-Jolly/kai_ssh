6ea3b4423e5f394987e56f0ceb800d3e537de162
Jolly <52607257+lzy-Jolly@users.noreply.github.com>
2024-09-25 23:31:49 +1000

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 
-# 检查别名是否已经存在，避免重复添加
-if ! grep -q "alias Cpl=" ~/.bashrc; then
-    echo "alias Cpl='rsync -avzP'" >> ~/.bashrc
-    echo "Added alias Cpl to ~/.bashrc"
-fi
-
-if ! grep -q "alias sdpcd=" ~/.bashrc; then
-    echo "alias sdpcd='docker start pcd'" >> ~/.bashrc
-    echo "Added alias sdpcd to ~/.bashrc"
-fi
-
-if ! grep -q "alias bspcd=" ~/.bashrc; then
-    echo "alias bspcd='docker exec -it pcd /bin/bash'" >> ~/.bashrc
-    echo "Added alias bspcd to ~/.bashrc"
-fi
-
-if ! grep -q "alias jy=" ~/.bashrc; then
-    echo "alias jy='tar -xvzf'" >> ~/.bashrc
-    echo "Added alias jy to ~/.bashrc"
-fi
+alias cpl="rsync -avzP "
+alias sdpcd="docker start pcd"
+alias bspcd="docker exec -it pcd /bin/bash"
+alias jy="tar -xvzf"
 
 # 立即使修改生效
 source ~/.bashrc
