7d9851271cc52f00c75748c9def7d8136a5bcec1
Jolly <52607257+lzy-Jolly@users.noreply.github.com>
2024-09-27 02:14:22 +1000

    Update dl_nuscenes.sh

--- dl_nuscenes.sh
+++ dl_nuscenes.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# 创建文件夹并进入
-mkdir -p /home/featurize/data/tmp
-cd /home/featurize/data/tmp
-
 # 定义函数记录时间
 time_elapsed() {
     start=$1
     end=$2
     elapsed=$(( end - start ))
-    echo "Download and processing time: $elapsed seconds"
+    echo "Elapsed time: $elapsed seconds"
 }
 
-# 检查第一个数据集是否已经下载
+# 创建目录并进入
+mkdir -p /home/featurize/data/tmp
+cd /home/featurize/data/tmp
+
+# 检查是否已经存在 v1.0-trainval_meta 文件夹
 if [ ! -d "/home/featurize/data/tmp/v1.0-trainval_meta" ]; then
     echo "Downloading first dataset..."
     start_time=$(date +%s)
@@ -24,27 +24,53 @@ if [ ! -d "/home/featurize/data/tmp/v1.0-trainval_meta" ]; then
     mv /home/featurize/data/tmp/v1.0-trainval_meta/* /home/featurize/data/nuscenes/v1.0-trainval/
     
     end_time=$(date +%s)
+    echo "Time for first dataset:"
     time_elapsed $start_time $end_time
 else
     echo "First dataset already downloaded, skipping..."
 fi
 
-# 检查第二个数据集是否已经下载
-if [ ! -d "/home/featurize/data/tmp/nuscenes-full" ]; then
+# 回到 tmp/nuscenes_test 目录
+mkdir -p /home/featurize/data/tmp/nuscenes_test
+cd /home/featurize/data/tmp/nuscenes_test
+
+# 检查是否已经存在 v1.0-trainval_meta 文件夹
+if [ ! -d "/home/featurize/data/tmp/nuscenes_test/v1.0-trainval_meta" ]; then
     echo "Downloading second dataset..."
     start_time=$(date +%s)
     
     # 下载第二个数据集
+    featurize dataset extract e0f18708-b871-42ed-9659-140077b94983
+    
+    # 剪切文件到目标目录
+    mv /home/featurize/data/tmp/nuscenes_test/v1.0-trainval_meta/* /home/featurize/data/nuscenes_test/
+    
+    end_time=$(date +%s)
+    echo "Time for second dataset:"
+    time_elapsed $start_time $end_time
+else
+    echo "Second dataset already downloaded, skipping..."
+fi
+
+# 回到 tmp 目录
+cd /home/featurize/data/tmp
+
+# 检查是否已经存在 nuscenes-full 文件夹
+if [ ! -d "/home/featurize/data/tmp/nuscenes-full" ]; then
+    echo "Downloading third dataset..."
+    start_time=$(date +%s)
+    
+    # 下载第三个数据集
     featurize dataset extract 7c9beef0-b2e6-4582-96f6-d82b3db1e89f
     
     # 移动文件到目标目录
     mv /home/featurize/data/tmp/nuscenes-full/* /home/featurize/data/nuscenes/v1.0-trainval/
     
     end_time=$(date +%s)
+    echo "Time for third dataset:"
     time_elapsed $start_time $end_time
 else
-    echo "Second dataset already downloaded, skipping..."
+    echo "Third dataset already downloaded, skipping..."
 fi
 
-# 提示下载完成
-echo "Both datasets have been processed."
+echo "All datasets have been processed."
