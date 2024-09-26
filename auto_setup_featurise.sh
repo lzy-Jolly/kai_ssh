ba64f7ded3da17ab2dceec2e6317ce08aa43a963
Jolly <52607257+lzy-Jolly@users.noreply.github.com>
2024-09-26 16:25:49 +1000

    Update auto_setup_featurise.sh

--- auto_setup_featurise.sh
+++ auto_setup_featurise.sh
@@ -2,7 +2,23 @@
 #!/bin/bash
 
 # 复制所有文件从 /home/featurize/work 到 /home/featurize
-cp -r /home/featurize/work/* /home/featurize/
+# cp -r /home/featurize/work/* /home/featurize/
+
+# 检查是否成功复制了 pcd_jolly_1.1.tar
+# if [ -f "/home/featurize/pcd_jolly_1.1.tar" ]; then
+#     echo "pcd_jolly_1.1.tar found. Loading into Docker..."
+#     # 加载 pcd_jolly_1.1.tar 文件到 Docker
+#     docker load -i /home/featurize/pcd_jolly_1.1.tar
+#     echo "Docker image loaded successfully."
+# else
+#     echo "Error: pcd_jolly_1.1.tar not found."
+# fi
+
+
+#!/bin/bash
+
+# 复制除了 cuda_11.6.2.tar 以外的所有文件和文件夹从 /home/featurize/work 到 /home/featurize
+rsync -av --exclude='cuda_11.6.2.tar' /home/featurize/work/ /home/featurize/
 
 # 检查是否成功复制了 pcd_jolly_1.1.tar
 if [ -f "/home/featurize/pcd_jolly_1.1.tar" ]; then
